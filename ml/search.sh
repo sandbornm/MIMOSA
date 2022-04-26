a37598be330ef004c94560d808a3ec7d977251f1
Zach Stoebner <zstoebner25@gmail.com>
2022-04-26 16:36:14 -0500

    tune pls

---
+++ ml/search.sh
@@ -0,0 +1,4 @@
+X=/Users/Zachary/Desktop/School/Vandy/Masters/M_Year1/Spring_2022/CS_8395_04/project/MIMOSA/ml/data/imgs
+Y=/Users/Zachary/Desktop/School/Vandy/Masters/M_Year1/Spring_2022/CS_8395_04/project/MIMOSA/ml/data/config_multilabel.csv
+
+caffeinate python3 search.py -x $X  -y $Y -g 0
