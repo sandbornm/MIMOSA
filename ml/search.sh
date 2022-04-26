24f580322a6cebfeb9d8038f5ac946ea38d03c5e
Zach Stoebner <zstoebner25@gmail.com>
2022-04-26 18:29:06 -0500

    tune pls

--- ml/search.sh
+++ ml/search.sh
@@ -1,4 +1,7 @@
-X=/Users/Zachary/Desktop/School/Vandy/Masters/M_Year1/Spring_2022/CS_8395_04/project/MIMOSA/ml/data/imgs
-Y=/Users/Zachary/Desktop/School/Vandy/Masters/M_Year1/Spring_2022/CS_8395_04/project/MIMOSA/ml/data/config_multilabel.csv
+#X=/Users/Zachary/Desktop/School/Vandy/Masters/M_Year1/Spring_2022/CS_8395_04/project/MIMOSA/ml/data/imgs
+#Y=/Users/Zachary/Desktop/School/Vandy/Masters/M_Year1/Spring_2022/CS_8395_04/project/MIMOSA/ml/data/config_multilabel.csv
 
-python3 search.py -x $X  -y $Y -g 0
+X=/home/michael2/MIMOSA/ml/data/imgs
+Y=/home/michael2/MIMOSA/ml/data/config_multilabel.csv
+
+python3 search.py -x $X  -y $Y -g 2
